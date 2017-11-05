7cc289abf48bf0daa561b7f38826c6112f88f6f1
Matt Tingen <matt.tingen@gmail.com>
2017-11-05 18:01:55 -0500

    Fix execution order
    http://thejameskyle.com/babel-plugin-ordering.html

--- src/index.js
+++ src/index.js
@@ -8,36 +8,41 @@ export default function({ types: t }) {
       this.opts = Object.assign({}, defaults, state.opts);
     },
     visitor: {
-      ClassMethod(path, state) {
-        if (path.node.key.name !== 'render') {
-          return;
-        }
+      Program(path) {
+        const { opts } = this;
+        path.traverse({
+          ClassMethod(path) {
+            if (path.node.key.name !== 'render') {
+              return;
+            }
 
-        const cls = path.findParent(path => path.isClassDeclaration());
-        if (!this.opts.superClasses.includes(cls.node.superClass.name)) {
-          return;
-        }
+            const cls = path.findParent(path => path.isClassDeclaration());
+            if (!opts.superClasses.includes(cls.node.superClass.name)) {
+              return;
+            }
 
-        const paramNames = ['props', 'state'];
-        const params = path.get('params').slice(0, paramNames.length);
-        const body = path.get('body');
+            const paramNames = ['props', 'state'];
+            const params = path.get('params').slice(0, paramNames.length);
+            const body = path.get('body');
 
-        if (params.length) {
-          const declarators = params.map((param, i) => {
-            const name = paramNames[i];
-            return t.variableDeclarator(
-              param.node,
-              t.memberExpression(t.thisExpression(), t.identifier(name))
-            );
-          });
+            if (params.length) {
+              const declarators = params.map((param, i) => {
+                const name = paramNames[i];
+                return t.variableDeclarator(
+                  param.node,
+                  t.memberExpression(t.thisExpression(), t.identifier(name))
+                );
+              });
 
-          const declaration = t.variableDeclaration('const', declarators);
-          path.get('body').unshiftContainer('body', declaration);
+              const declaration = t.variableDeclaration('const', declarators);
+              path.get('body').unshiftContainer('body', declaration);
 
-          params.forEach(param => {
-            param.remove();
-          });
-        }
+              params.forEach(param => {
+                param.remove();
+              });
+            }
+          }
+        });
       }
     }
   };
